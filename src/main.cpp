11f91af1ccf0aae8f6055bf1585bb28626a1e447
Nirmal Diaz <nirmaldiaz@gmail.com>
2021-09-24 09:32:07 +0530

    Added: Buzzer and LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <Keypad.h>
-#include <Servo.h> 
+#include <Servo.h>
+
+#define RED_LED A0
+#define GREEN_LED A1
+#define BUZZER 10
 
 #define DB_SIZE 5
-#define OPTIONS_MASTER "1.Tag management #.Cancel"
-#define OPTIONS_UNREGISTERED_TAG "1.Register #.Cancel"
-#define OPTIONS_REGISTERED_TAG "1.Unregister 2.Edit tag #.Cancel"
-#define OPTIONS_EDIT_TAG "1.Activate 2.Deactivate 3.Make master #.Cancel"
+#define DELAY 2000
+#define OPTIONS_MASTER "1.Tag management #.Quit"
+#define OPTIONS_UNREGISTERED_TAG "1.Register #.Quit"
+#define OPTIONS_REGISTERED_TAG "1.Unregister 2.Edit tag #.Quit"
+#define OPTIONS_EDIT_TAG_ACTIVE "1.Deactivate 2.Make master #.Quit"
+#define OPTIONS_EDIT_TAG_INACTIVE "1.Activate 2.Make master #.Quit"
 
 //DEFINE DATA STRUCTURES
 class TagDatum {
@@ -90,7 +96,19 @@ short getNextEmptyIndex() {
     return -1;
 }
 
+void abortSetup(String line1, String line2) {
+    print(line1, line2);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    delay(DELAY);
+}
+
 void setup() {
+    //SETUP PINS
+    pinMode(RED_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(BUZZER, OUTPUT);
+
     //SETUP LIBRARIES
     Serial.begin(9600);
     Serial.println("Starting system...");
@@ -114,12 +132,18 @@ void loop() {
     if (tagIndex == -1) {
         //CASE: Tag is not registered in the database
         print("Tag is not registered", "Access denied");
-        delay(2000);
+        tone(BUZZER, 500);
+        digitalWrite(RED_LED, HIGH);
+        delay(5000);
+        noTone(BUZZER);
+        digitalWrite(RED_LED, LOW);
     } else if (tagData[tagIndex].isActive) {
         //CASE: Tag is registered in the database and is active
         if (tagData[tagIndex].isMaster) {
             //CASE: Tag is the master tag
             waitForKey1: print("MASTER MENU: Choose an option", OPTIONS_MASTER);
+            digitalWrite(RED_LED, HIGH);
+            digitalWrite(GREEN_LED, HIGH);
             char key = keypad.waitForKey();
             if (key == '1') {
                 //CASE: Edit database
@@ -136,30 +160,29 @@ void loop() {
                         if (newIndex == -1) {
                             //CASE: Database is full
                             print("Database is full", "Choose another option");
-                            delay(2000);
+                            delay(DELAY);
                             goto waitForKey2;
                         } else {
                             //CASE: Tag registered successfully
                             tagData[newIndex] = TagDatum(id, false, false);
                             print("Tag registered successfully", "Please wait");
-                            delay(2000);
+                            delay(DELAY);
                             tagIndex = newIndex;
                             goto waitForKey3;
                         }
                     } else if (key == '#') {
                         //CASE: Cancel setup
-                        print("Aborting setup", "Please wait");
-                        delay(2000);
+                        abortSetup("Aborting setup", "Please wait");
                     } else {
                         //CASE: Undefined option selected
                         print("No option for the key " + String(key), "Try again");
-                        delay(2000);
+                        delay(DELAY);
                         goto waitForKey2;
                     }
                 } else if (tagIndex == findMasterIndex()) {
                     //CASE: Tag is the master tag
                     print("Cannot edit the master tag itself", "Try with another tag");
-                    delay(2000);
+                    delay(DELAY);
                     goto waitForTag1;
                 } else {
                     //CASE: Tag is registered
@@ -167,77 +190,82 @@ void loop() {
                     char key = keypad.waitForKey();
                     if (key == '1') {
                         //CASE: Unregister tag
-                        tagData[tagIndex] = TagDatum(); 
-                        print("Tag removed successfully", "Please wait");
-                        delay(2000);
+                        tagData[tagIndex] = TagDatum();
+                        abortSetup("Tag removed successfully", "Quitting setup");
                     } else if (key == '2') {
                         //CASE: Update tag properties
-                        waitForKey4: print("EDIT TAG MENU: Choose an option", OPTIONS_EDIT_TAG);
+                        waitForKey4: if (tagData[tagIndex].isActive) {
+                            print("TAG MENU: Choose an option", OPTIONS_EDIT_TAG_ACTIVE);
+                        } else {
+                            print("TAG MENU: Choose an option", OPTIONS_EDIT_TAG_INACTIVE);
+                        }
                         char key = keypad.waitForKey();
-                        if (key == '1') {
-                            //CASE: Activate tag
-                            tagData[tagIndex].isActive = true;
-                            print("Tag activated successfully", "Continue editing tag");
-                            delay(2000);
-                            goto waitForKey4;
-                        } else if (key == '2') {
+                        if (key == '1' && tagData[tagIndex].isActive) {
                             //CASE: Deactivate tag
                             tagData[tagIndex].isActive = false;
-                            print("Tag deactivated successfully", "Continue editing tag");
-                            delay(2000);
+                            print("Tag deactivated successfully", "Please continue");
+                            delay(DELAY);
                             goto waitForKey4;
-                        } else if (key == '3') {
+                        } else if (key == '1' && !tagData[tagIndex].isActive) {
+                            //CASE: Activate tag
+                            tagData[tagIndex].isActive = true;
+                            print("Tag activated successfully", "Please continue");
+                            delay(DELAY);
+                            goto waitForKey4;
+                        } else if (key == '2') {
                             //CASE: Make tag the master
                             tagData[findMasterIndex()].isMaster = false;
                             tagData[tagIndex].isActive = true;
                             tagData[tagIndex].isMaster = true;
-                            print("Master tag is replaced successfully", "Please wait");
-                            delay(2000);
+                            abortSetup("Master tag is replaced successfully", "Quitting setup");
                         }else if (key == '#') {
                             //CASE: Cancel setup
-                            print("Aborting setup", "Please wait");
-                            delay(2000);
+                            abortSetup("Aborting setup", "Please wait");
                         } else {
                             //CASE: Undefined option selected
                             print("No option for the key " + String(key), "Try again");
-                            delay(2000);
+                            delay(DELAY);
                             goto waitForKey4;
                         }
                     } else if (key == '#') {
                         //CASE: Cancel setup
-                        print("Aborting setup", "Please wait");
-                        delay(2000);
+                        abortSetup("Aborting setup", "Please wait");
                     } else {
                         //CASE: Undefined option selected
                         print("No option for the key " + String(key), "Try again");
-                        delay(2000);
+                        delay(DELAY);
                         goto waitForKey3;
                     }
                 }
             } else if (key == '#') {
                 //CASE: Cancel setup
-                print("Aborting setup. Please wait", "");
-                delay(2000);
+                abortSetup("Aborting setup", "Please wait");
             } else {
                 //CASE: Undefined option selected
                 print("No option for the key " + String(key), "Try again");
-                delay(2000);
+                delay(DELAY);
                 goto waitForKey1;
             }
         } else {
             //CASE: Tag is not the master tag
             servo.write(180);
             print("Access granted", "Have a nice day!");
-            delay(2000);
+            digitalWrite(GREEN_LED, HIGH);
+            delay(DELAY);
             print("The door will be locked down soon", "Hurry!");
             delay(10000);
             servo.write(0);
-            print("Locking", "Please wait");
-            delay(2000);
+            print("Locking down", "Please wait");
+            digitalWrite(GREEN_LED, LOW);
+            delay(DELAY);
         }
     } else {
         //CASE: Tag is registered in the database and is not active
         print("Tag has no access permissions", "Access denied");
-        delay(2000);
+        tone(BUZZER, 10000);
+        digitalWrite(RED_LED, HIGH);
+        delay(5000);
+        noTone(BUZZER);
+        digitalWrite(RED_LED, LOW);
     }
 }
